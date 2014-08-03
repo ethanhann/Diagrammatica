b53699199977de95956e7dcb9528326f3efc887f
Ethan Hann <ethanhann@gmail.com>
2014-08-02 23:59:06 -0400

    Separate column chart's implementation from its API

--- src/js/column.js
+++ src/js/column.js
@@ -1,15 +1,16 @@
 'use strict';
 /* global d3: false, check: false, ChartBase */
 /* exported column */
-var column = function (selection, data) {
-    selection = check.string(selection) ? d3.select(selection) : selection;
-    var chart = new ChartBase(selection, 'column');
-    var config = chart.config;
+
+var ColumnBase = function (selection, data) {
+    selection = this.selection = check.string(selection) ? d3.select(selection) : selection;
+    var chart = this.chart = new ChartBase(selection, 'column');
+    var config = this.config = chart.config;
 
     // ------------------------------------------------------------------------
     // Scales and axes
     // ------------------------------------------------------------------------
-    function updateX() {
+    this.updateX = function () {
         chart.xScale = d3.scale.ordinal()
             .domain(data.map(function (d) {
                 return d.name;
@@ -18,11 +19,10 @@ var column = function (selection, data) {
         chart.xAxis = d3.svg.axis()
             .scale(chart.xScale)
             .orient('bottom');
-    }
+    };
+    this.updateX();
 
-    updateX();
-
-    function updateY() {
+    this.updateY = function () {
         chart.yScale = d3.scale.linear()
             .domain([0, d3.max(data, function (d) {
                 return d.value;
@@ -31,9 +31,8 @@ var column = function (selection, data) {
         chart.yAxis = d3.svg.axis()
             .scale(chart.yScale)
             .orient('left');
-    }
-
-    updateY();
+    };
+    this.updateY();
 
     // ------------------------------------------------------------------------
     // SVG
@@ -78,91 +77,80 @@ var column = function (selection, data) {
         .attr('fill', function (d) {
             return chart.colors(d.value);
         });
+};
 
-//    var tooltipTemplate = function(d) {
-//        return d.value;
-//    };
-//
-//    if(check.defined(d3.tip)) {
-//        var tip = d3.tip()
-//            .attr('class', 'd3-tip')
-//            .offset([-10, 0])
-//            .html(tooltipTemplate);
-//        svg.call(tip);
-//        svg.selectAll('.column')
-//            .on('mouseover', tip.show)
-//            .on('mouseout', tip.hide);
-//    }
+var column = function (selection, data) {
+    var columnBase = new ColumnBase(selection, data);
 
     // ------------------------------------------------------------------------
     // Update/re-render
     // ------------------------------------------------------------------------
-    function update(newData) {
+    var update = columnBase.chart.update = function (newData) {
         data = check.defined(newData) ? newData : data;
 
-        updateY();
-        chart.renderArea.select('.y.axis')
+        columnBase.updateY();
+        columnBase.chart.renderArea.select('.y.axis')
             .transition()
             .duration(1000)
-            .call(chart.yAxis);
+            .call(columnBase.chart.yAxis);
 
-        updateX();
-        chart.renderArea.select('.x.axis')
+        columnBase.updateX();
+        columnBase.chart.renderArea.select('.x.axis')
             .transition()
             .duration(1000)
-            .call(chart.xAxis)
-            .attr('transform', 'translate(0,' + config.paddedHeight() + ')');
+            .call(columnBase.chart.xAxis)
+            .attr('transform', 'translate(0,' + columnBase.config.paddedHeight() + ')');
 
-        chart.renderArea.selectAll('.column')
+        columnBase.chart.renderArea.selectAll('.column')
             .data(data)
             .transition()
             .duration(1000)
             .ease('linear')
             .attr('x', function (d) {
-                return chart.xScale(d.name);
+                return columnBase.chart.xScale(d.name);
             })
             .attr('y', function (d) {
-                return chart.yScale(d.value);
+                return columnBase.chart.yScale(d.value);
             })
-            .attr('width', chart.xScale.rangeBand())
+            .attr('width', columnBase.chart.xScale.rangeBand())
             .attr('height', function (d) {
-                return config.paddedHeight() - chart.yScale(d.value);
+                return columnBase.config.paddedHeight() - columnBase.chart.yScale(d.value);
             });
-    }
-    chart.update = update;
+    };
 
     // ------------------------------------------------------------------------
     // Properties
     // ------------------------------------------------------------------------
-    update.width = function (value) {
-        return chart.width(value, function () {
-            updateX();
-            selection.select('.x.axis .label')
+    update.height = function (value) {
+        return columnBase.chart.height(value, function () {
+            columnBase.updateY();
+            columnBase.selection.select('.y.axis .label')
                 .transition()
                 .duration(1000)
-                .attr('x', config.paddedWidth() / 2)
+                .attr('y', columnBase.config.margin.left * -1)
+                .attr('x', (columnBase.config.paddedHeight() / 2) * -1);
+        });
+    };
+
+    update.width = function (value) {
+        return columnBase.chart.width(value, function () {
+            columnBase.updateX();
+            columnBase.selection.select('.x.axis .label')
+                .transition()
+                .duration(1000)
+                .attr('x', columnBase.config.paddedWidth() / 2)
                 .attr('y', 28);
         });
     };
 
-    update.height = function (value) {
-        return chart.height(value, function () {
-            updateY();
-            selection.select('.y.axis .label')
-                .transition()
-                .duration(1000)
-                .attr('y', config.margin.left * -1)
-                .attr('x', (config.paddedHeight() / 2) * -1);
-        });
-    };
-
     update.yAxisLabelText = function (value) {
-        return chart.yAxisLabelText(value);
+        return columnBase.chart.yAxisLabelText(value);
     };
 
     update.xAxisLabelText = function (value) {
-        return chart.xAxisLabelText(value);
+        return columnBase.chart.xAxisLabelText(value);
     };
 
     return update;
 };
+

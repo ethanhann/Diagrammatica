b970726e9df00e930dc02cf5faa589d10aa065de
Ethan Hann <ethanhann@gmail.com>
2014-08-29 11:51:17 -0400

    Wrap angular module in a closure and use $window

--- src/angular-module/module.js
+++ src/angular-module/module.js
@@ -1,41 +1,42 @@
-angular.module('diagrammatica', []);
-
-angular.module('diagrammatica').factory('diagrammatica', [function () {
+(function (angular) {
     'use strict';
-    if (angular.isUndefined(window.diagrammatica)) {
-        console.log('The Diagrammatica library is required.');
-    }
-    return window.diagrammatica;
-}]);
+    var dma = angular.module('diagrammatica', []);
 
-angular.module('diagrammatica').directive('dmaHeatMap', ['$window', 'diagrammatica', function ($window, diagrammatica) {
-    'use strict';
-    return {
-        restrict: 'A',
-        scope: {
-            data: '=dmaHeatMap',
-            from: '=?dmaHeatMapFrom',
-            to: '=?dmaHeatMapTo'
-        },
-        link: function (scope, element) {
-            var chart = diagrammatica.heatMap(element[0], scope.data).width(element.width());
-            $window.onresize = function () {
-                scope.$apply();
-            };
-            scope.$watch(function () {
-                return angular.element($window)[0].innerWidth;
-            }, function () {
-                chart.width(element.width())();
-            });
-            scope.$watch('data', function () {
-                chart(scope.data);
-            });
-            scope.$watch('from', function () {
-                chart.fromX(scope.from)();
-            });
-            scope.$watch('to', function () {
-                chart.toX(scope.to)();
-            });
+    dma.factory('diagrammatica', ['$window', function ($window) {
+        if (angular.isUndefined($window.diagrammatica)) {
+            console.log('The Diagrammatica library is required.');
         }
-    };
-}]);
+        return $window.diagrammatica;
+    }]);
+
+    dma.directive('dmaHeatMap', ['$window', 'diagrammatica', function ($window, diagrammatica) {
+        return {
+            restrict: 'A',
+            scope: {
+                data: '=dmaHeatMap',
+                from: '=?dmaHeatMapFrom',
+                to: '=?dmaHeatMapTo'
+            },
+            link: function (scope, element) {
+                var chart = diagrammatica.heatMap(element[0], scope.data).width(element.width());
+                $window.onresize = function () {
+                    scope.$apply();
+                };
+                scope.$watch(function () {
+                    return angular.element($window)[0].innerWidth;
+                }, function () {
+                    chart.width(element.width())();
+                });
+                scope.$watch('data', function () {
+                    chart(scope.data);
+                });
+                scope.$watch('from', function () {
+                    chart.fromX(scope.from)();
+                });
+                scope.$watch('to', function () {
+                    chart.toX(scope.to)();
+                });
+            }
+        };
+    }]);
+})(angular);
